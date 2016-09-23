f980c2830bbbe1bb4e7387e341d3f409e9cc2d81
amn-rauniyar <amn.rauniyar@gmail.com>
2016-09-23 15:21:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.send(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 app.get('/article1', function (req, res){
