219840facd66ceafb1ca8ad26d4b96f504549a5d
amn-rauniyar <amn.rauniyar@gmail.com>
2016-09-23 15:24:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ app.get('/article1', function (req, res){
     
 });
 
+app.get('/article-two',function(req,res){
+    res.send("articel-two will be served here");
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
