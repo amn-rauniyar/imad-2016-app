2de4d347c0434ec97b300cc760f276dfcc3be61f
amn-rauniyar <amn.rauniyar@gmail.com>
2016-09-29 01:01:17 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,7 @@
+var count=document.getElementById('counter');
+button.onclick=function(){
+    counter=counter + 1;
+    var span = document.getElementId('count');
+    span.innerHTML= counter.toString();
+    
+};
