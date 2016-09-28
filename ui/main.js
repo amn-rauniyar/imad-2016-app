3c28c7c95a807fce1e5c21a43fad2d68dd69e229
amn-rauniyar <amn.rauniyar@gmail.com>
2016-09-28 22:17:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 function moveright(){
     marginLeft=marginLeft + 10;
-    img.style.margin=marginLeft + 'px';
+    img.style.marginLeft=marginLeft + 'px';
 }
 img.onclick=function(){
     var interval=setInterval(moveright,200);
