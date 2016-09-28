c083bb16b626cb490c5f306af5130838c7becc1e
amn-rauniyar <amn.rauniyar@gmail.com>
2016-09-28 20:11:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,63 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var articleone={
+    title:'Article-One',
+    heading:'Article',
+    time:'29th Seppt',
+    content:`
+    <p>
+                    Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!!Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!!
+                </p>
+                  <p>
+                    Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!!Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!!
+                </p>
+                
+                  <p>
+                    Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!!Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!! Welcome to page 2 hello how are you??!!
+                </p>
+    `
+};
+function createTemp(data){
+ var title=data.title;
+ var heading=data.heading;
+ var time=data.time;
+ var content=data.content;
+    
+
+var template=`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="widtht=device-width, initial-scale-1" />
+         <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            
+            <div>
+            <div>
+                <a href="/">HOME</a>
+            </div>
+            <h3>
+               ${heading}
+            </h3>
+            <div>
+                ${time}
+            </div>
+                <div class="container">
+               ${content}
+                </div>
+            </div>
+        </body>
+
+</html>
+`;
+return template;
+}
+
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -18,7 +75,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article1', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemp(articleone));
     
 });
 
