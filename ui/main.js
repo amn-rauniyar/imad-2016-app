b6ab973ebbdd4499a80f3b42d433aef611ff721e
amn-rauniyar <amn.rauniyar@gmail.com>
2016-09-28 22:01:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 //change the text of main page
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+
+//move image
+
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.margin='100px';
+};
