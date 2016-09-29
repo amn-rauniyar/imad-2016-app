6c73a33d3a668edd174ffc84fd2c711357abb751
amn-rauniyar <amn.rauniyar@gmail.com>
2016-09-29 21:46:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ button.onclick = function (){
 };
 
 var nameInput=document.getElementById('name');
-var name = nameInput.value;
+var name1 = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick= function(){
 var names = ['name1','name2','name3','name4'];
@@ -17,5 +17,5 @@ for(var i=0;i<names.length;i++){
     list += '<li>' + names[i] + '<li>';
 }
 var ul=document.getElementById('namelist');
-ul.innerHTML = 'list';
+ul.innerHTML = list;
 };
